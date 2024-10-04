6e89de1d1e5fb3605da67e74a947f7b943eb3c2a
joao.goroncy <joao.goroncy@viasoft.com.br>
2024-10-03 21:54:32 -0300

    03/10/24 - Aula referente ao backend do projeto

--- mater-play-backend/src/app.module.ts
+++ mater-play-backend/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
---
+++ mater-play-backend/src/entities/movie-entity.ts
@@ -0,0 +1,7 @@
+
+export class Movie {
+    id: string;
+    tittle: string;
+    description: string;
+    ageRating: string;
+}
